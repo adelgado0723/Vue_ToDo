ab2647a4ac410c67069bbb661755395ba351aea1
Andres Delgado <adelgado0723@gmail.com>
2019-06-02 23:44:33 -0400

    At 29:05 into the tutorial - Vue Class Binding

---
+++ src/components/Skills.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <div class="holder">
+      <ul>
+        <li v-for="(data, index) in skills" :key="index">{{ index }}. {{ data.skill }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+  data() {
+    return {
+      skills: [{ skill: 'Vue.js' }, { skill: 'Frontend Developer' }],
+      name: 'Andy Knight',
+      btnState: true,
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style src="./Skills.css"scoped>
+</style>
